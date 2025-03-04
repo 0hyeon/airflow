aaf0e1c26683c442e838d0d4c003f3b330984b7c
0hyeon <djdjdjk2006@naver.com>
2025-03-04 13:07:03 +0900

    fix: error

--- lecture_2/dags/test-dag.py
+++ lecture_2/dags/test-dag.py
@@ -4,17 +4,13 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id='test_dag',
-    start_date=datetime.datetime(2023,11,19),
-    schedule_interval='@daily'
-    ) as dag:
+    dag_id="test_dag",
+    start_date=datetime.datetime(2025, 3, 2),
+    schedule_interval="@daily",
+) as dag:
 
-    step1 = BashOperator(
-        task_id='print_date',
-        bash_command='date')
+    step1 = BashOperator(task_id="print_date", bash_command="date")
 
-    step2 = BashOperator(
-        task_id='echo',
-        bash_command='echo 123')
+    step2 = BashOperator(task_id="echo", bash_command="echo 123")
 
     step1 >> step2
