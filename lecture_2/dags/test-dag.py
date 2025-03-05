150130c6ac8498226c9f3dd8a11212d659ae1876
0hyeon <djdjdjk2006@naver.com>
2025-03-05 20:49:33 +0900

    fix: error

--- lecture_2/dags/test-dag.py
+++ lecture_2/dags/test-dag.py
@@ -8,6 +8,7 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.models import Variable
 
 # 날짜 계산 (월요일이면 3일치 가져오기, 아니면 1일치)
 current_date = datetime.now()
@@ -17,7 +18,7 @@ yesterday = current_date - timedelta(days=1)
 from_date = "2025-03-01"
 to_date = "2025-03-01"
 
-TOKEN = "eyJhbGciOiJBMjU2S1ciLCJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwidHlwIjoiSldUIiwiemlwIjoiREVGIn0.lBO3HU5TLSD-AOli7dqnczeHPE0b82fkeiyYnJ6iIzza7LGuiC8A6g.rrlL8vXwLFade5ez.xVjNZ4SEb4f8Yz_48b8Wfn1mrc77FZ74n7KZwu5rRK-8RGDqYSDCGJgLzWA9Pth1dKjAl_N7vhrDLqrUXR_r5kYHOHPvH7fnYaBHXC0kSweeLbOs8isdZpQLG8Izv8WGg4TvYzuS58slvpixjvzRVvWSiA4YXJxNHlBDjE9SkdNSz5kB7GJkwI8xCg68iH_WBswZvm0CFzwb5QIvnn4bqHfxhfSffv_XKiozX75JYmL9Iz5oDYVPlUm2tCsezmu16Z_Pgqe7RVtojcHxhBMW0Ceyal9LN91S7A6NqFTDGTAlwAZSeumdPGWW2Kr02Dj4OZcrIX7Cvp1q79Ksu3Q0Tcl0A8RB19C78r9rvj54T_9OvsrNvZoqOwtJhlsVk_xCLzdll5BGwsfaIvaZgmRCO7hgDQoyd4pe_DOIlLjQvzcX3vdggsJZfDieYSM_V3qa1UGSh7vcNo7C5fZm23oAdc0rUeSLImHs3unkPvAipjaTiHcveo7oXz3iB3-NQOH9h5FIkfILYDF_KCVnHk5C4T0.kxXA0zGdBOH4B6D6-g17lA"
+TOKEN = Variable.get("TOKEN")
 
 HEADERS = {"accept": "application/json", "authorization": f"Bearer {TOKEN}"}
 
@@ -34,9 +35,9 @@ URLS = {
 S3_BUCKET = "fc-practice2"
 S3_KEY_PREFIX = "apps_flyer_data/"
 
-AWS_ACCESS_KEY = "AKIAR6TQB2EGZHHVEJ7C"
-AWS_SECRET_KEY = "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"
-AWS_DEFAULT_REGION = "ap-northeast-2"
+AWS_ACCESS_KEY = Variable.get("AWS_ACCESS_KEY")
+AWS_SECRET_KEY = Variable.get("AWS_SECRET_KEY")
+AWS_DEFAULT_REGION = Variable.get("AWS_DEFAULT_REGION")
 
 
 # **S3 저장 함수**
